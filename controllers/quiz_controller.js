09774b17b3db53756ecaac5f12036a877bef0d09
Almeida95 <martinezalmeida95@gmail.com>
2016-05-23 16:54:22 +0200

    Formatos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,25 +14,62 @@ exports.index = function(req, res, next){
     })
     .catch(function(error){
       next(error);
-    });}
-    else{
-      models.Quiz.findAll().then(function(quizzes){
-        res.render("quizzes/index.ejs",{quizzes:quizzes});
-      })
-      .catch(function(error){
-        next(error);
-      });
-    }
-  };
+    });
+  }
+
+  if(req.params.format=== 'json') {
+    models.Quiz.findAll()
+    .then(function(quizzes){
+      res.json('quizzes/index.ejs',{ quizzes:quizzes});
+    })
+    .catch(function(error){
+      next(error);
+    });
+  }
+  else {
+    models.Quiz.findAll()
+    .then(function(quizzes){
+      res.render('quizzes/index.ejs',{quizzes:quizzes});
+    })
+    .catch(function(error){
+      next(error);
+    });
+  }
+};
 
 
 // GET /quizzes/:id
 exports.show = function(req, res, next) {
-
-	var answer = req.query.answer || '';
-
-	res.render('quizzes/show', {quiz: req.quiz,
-								answer: answer});
+  if(req.params.format=== 'json') {
+    models.Quiz.findById(req.params.quizId)
+    .then(function(quiz){
+      if(quiz){
+        var answer = req.query.answer || '';
+        res.json('quizzes/show.ejs',{ quiz: req.quiz, answer: answer});
+      }
+      else{
+        throw new Error('No hay preguntas en la Base de Datos');
+      }
+    })
+    .catch(function(error){
+      next(error);
+    });
+  }
+  else {
+    models.Quiz.findAll()
+    .then(function(quiz){
+      if(quiz){
+        var answer = req.query.answer || '';
+        res.render('quizzes/show.ejs',{quiz: req.quiz, answer: answer});
+    }
+    else{
+      throw new Error('No hay preguntas en la Base de Datos');
+    }
+  })
+    .catch(function(error){
+      next(error);
+    });
+  }
 };
 
  // GET /quizzes/:id/check
